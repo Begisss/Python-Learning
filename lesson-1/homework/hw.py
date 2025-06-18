57b661b446e228b6bbd47728dba7faaa603a9988
Begisss <abdiramanovbegis@gmail.com>
2025-06-18 16:04:20 +0500

    Create hw.py

---
+++ lesson-1/homework/hw.py
@@ -0,0 +1,26 @@
+side = 5
+perimeter = 4 * side
+area = side * side
+print("Perimeter:", perimeter)
+print("Area:", area)
+
+import math
+diameter = 10
+length = math.pi * diameter
+print("Circumference (Length):", length)
+
+a = 8
+b = 12
+mean = (a + b) / 2
+print("Mean:", mean)
+
+a = 4
+b = 7
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+print("Sum:", sum_ab)
+print("Product:", product_ab)
+print("Square of a:", square_a)
+print("Square of b:", square_b)
